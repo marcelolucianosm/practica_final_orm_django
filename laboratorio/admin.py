1fab7222626e412b739a8d8d66cf528108a2f97f
msanchezm <msanchezm@mtt.gob.cl>
2024-11-18 16:05:19 -0300

    commit cambios

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -1,3 +1,21 @@
+
 from django.contrib import admin
+from .models import Laboratorio, DirectorGeneral,Producto
 
 # Register your models here.
+
+class LaboratorioAdmin(admin.ModelAdmin):
+    list_display = ('nombre')
+    search_fields = ('nombre',)
+    
+class DirectorGeneralAdmin(admin.ModelAdmin):
+    list_display = ('nombre','laboratorio')
+    search_fields = ('nombre','laboratorio')
+    
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre','laboratorio')
+    search_fields = ('nombre','laboratorio')
+    
+admin.site.register(Laboratorio)
+admin.site.register(DirectorGeneral)
+admin.site.register(Producto)
--- laboratorio/models.py
+++ laboratorio/models.py
@@ -2,7 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Laboratorio(models.Model):
-    nombre = models.CharField(max_length=150)
+    laboratorio = models.CharField(max_length=50, blank=True, null=True)
+    
+    def __str__(self):
+        return self.laboratorio
+    
+class DirectorGeneral(models.Model):
+    nombre = models.CharField(max_length=50, blank=True, null=True)
+    laboratorio = models.OneToOneField(Laboratorio, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50, blank=True, null=True)
+    laboratorio = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
+    f_fabricacion = models.DateTimeField(null=False, blank=False, auto_now_add=True)
+    p_costo = models.FloatField(blank=False, null=False,)
+    p_venta = models.FloatField(blank=False, null=False,)
     
     def __str__(self):
         return self.nombre
