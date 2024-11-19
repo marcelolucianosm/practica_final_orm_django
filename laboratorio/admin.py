bcb0b58d96e2b1388546e10bb69735dc3781d3fa
msanchezm <msanchezm@mtt.gob.cl>
2024-11-19 12:16:00 -0300

    se configura administracion

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -5,19 +5,25 @@ from .models import Laboratorio, DirectorGeneral,Producto
 # Register your models here.
 
 class LaboratorioAdmin(admin.ModelAdmin):
-    list_display = ('id','nombre')
-    search_fields = ('id','nombre',)
+    list_display = ('id','laboratorio')
+    ordering = ('id',)
+    search_fields = ('id','laboratorio',)
+    #list_filter = ('id','laboratorio')
     
 
 class DirectorGeneralAdmin(admin.ModelAdmin):
     list_display = ('id','nombre','laboratorio')
+    ordering = ('id',)
     search_fields = ('id','nombre','laboratorio')
+    #list_filter = ('nombre','laboratorio')
 
     
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('id','nombre','laboratorio')
+    ordering = ('id',)
     search_fields = ('id','nombre','laboratorio')
+    list_filter = ('nombre','laboratorio')
     
-admin.site.register(Laboratorio)
-admin.site.register(DirectorGeneral)
-admin.site.register(Producto)
+admin.site.register(Laboratorio,LaboratorioAdmin)
+admin.site.register(DirectorGeneral,DirectorGeneralAdmin)
+admin.site.register(Producto,ProductoAdmin)
