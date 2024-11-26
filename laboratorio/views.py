7079f53f397e6563272f3107974c46a0364e20b1
msanchezm <msanchezm@mtt.gob.cl>
2024-11-26 11:17:36 -0300

    ultimos maquillajes

--- laboratorio/views.py
+++ laboratorio/views.py
@@ -1,3 +1,4 @@
+from django.forms import ValidationError
 from django.http.response import HttpResponseRedirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
@@ -14,7 +15,6 @@ from .forms import LaboratorioForm
 
 class LaboratorioView(ListView):
     model = Laboratorio
-
     template_name = "laboratorios/laboratorios.html"
     context_object_name = 'laboratorio'
     ordering = ['id']
@@ -24,35 +24,11 @@ class LaboratorioView(ListView):
         context["titulo"] = 'Página de Laboratorios'
         return context
     
-class LaboratorioAddView(CreateView):
-    model = Laboratorio
-    form_class = LaboratorioForm
-    template_name = "laboratorios/add_laboratorio.html"
-    fields = ['laboratorio', 'ciudad','pais']
-    success_url = reverse_lazy('laboratorio')
-    
-    def form_valid(self, form):
-        # You can add additional custom logic here before saving the object
-        laboratorio = form.save(commit=False)  # Do not save yet
-        # You could modify the item object here if needed
-        laboratorio.save()  # Now save the object
-        return super().form_valid(form)
-    
-    def form_invalid(self, form):
-        # Handle invalid form, perhaps log the error or add custom handling
-        return super().form_invalid(form)
-   
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
-    
 class LaboratorioUpdateView(UpdateView):
     model = Laboratorio
     template_name = "laboratorios/upd_laboratorio.html"
     fields = ['laboratorio', 'ciudad','pais']
     success_url = reverse_lazy('laboratorio')
-   
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -63,6 +39,7 @@ class LaboratorioUpdateView(UpdateView):
 class LaboratorioDeleteView(DeleteView):
     model = Laboratorio
     template_name = "laboratorios/del_laboratorio.html"
+    context_object_name = 'laboratorio'
     fields = ['laboratorio', 'ciudad','pais']
     success_url = reverse_lazy('laboratorio')
     
@@ -87,7 +64,7 @@ def add_laboratorio(request):
         else:
             messages.error(request, "Error al intentar crear el laboratorio, intente nuevamente.")
         
-        return render(request, "laboratorios/add_laboratorio.html", {"form": LaboratorioForm()})
+        return render(request, "laboratorios/add_laboratorio.html", {"form": LaboratorioForm()})   
     
     else:
         form = LaboratorioForm()
